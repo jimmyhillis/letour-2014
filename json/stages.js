8bfaa62407d14293353d86a74ece738b5ca30df9
Jimmy Hillis <jimmy@hillis.me>
2014-07-06 11:45:00 +0800

    Add JSON for stages and times as well as geojson points to map each track

---
+++ json/stages.js
@@ -0,0 +1,246 @@
+[
+  {
+    "date": "Saturday 5 July",
+    "stage": 1,
+    "distance": 191,
+    "location": "Leeds - Harrogate",
+    "time": {
+      "AEST": "8:30pm",
+      "ACST": "8:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Sunday 6 July",
+    "stage": 2,
+    "distance": 198,
+    "location": "York - Sheffield",
+    "time": {
+      "AEST": "8:30pm",
+      "ACST": "8:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Monday 7 July",
+    "stage": 3,
+    "distance": 159,
+    "location": "Cambridge - London",
+    "time": {
+      "ONLINE": "9:00pm AEST",
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+
+  {
+    "date": "Tuesday 8 July",
+    "stage": 4,
+    "distance": 164,
+    "location": "Le Touquet-Paris-Plage - Lille Métropole ",
+    "time": {
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Wednesday 9 July",
+    "stage": 5,
+    "distance": 156,
+    "location": "Ypres - Arenberg Porte du Hainaut ",
+    "time": {
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Thursday 10 July",
+    "stage": 6,
+    "distance": 194,
+    "location": "Arras - Reims",
+    "time": {
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Friday 11 July",
+    "stage": 7,
+    "distance": 233,
+    "location": "Épernay - Nancy",
+    "time": {
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Saturday 12 July",
+    "stage": 8,
+    "distance": 161,
+    "location": "Tomblaine - Gérardmer La Mauselaine",
+    "time": {
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Sunday 13 July",
+    "stage": 9,
+    "distance": 166,
+    "location": "Gérardmer - Mulhouse",
+    "time": {
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Monday 14 July",
+    "stage": 10,
+    "distance": 161,
+    "location": "Mulhouse - La Planche des Belles Filles",
+    "time": {
+      "ONLINE": "9:00pm AEST",
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm - SBS ONE"
+    }
+  },
+  {
+    "date": "Tuesday 15 July",
+    "rest": true
+  },
+  {
+    "date": "Wednesday 16 July",
+    "stage": 11,
+    "distance": 186,
+    "location": "Besançon - Oyonnax",
+    "time": {
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Thursday 17 July",
+    "stage": 12,
+    "distance": 183,
+    "location": "Bourg-en-Bresse - Saint-Étienne",
+    "time": {
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Friday 18 July",
+    "stage": 13,
+    "distance": 200,
+    "location": "Saint-Étienne  - Chamrousse",
+    "time": {
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Saturday 19 July",
+    "stage": 14,
+    "distance": 177,
+    "location": "Grenoble - Risoul",
+    "time": {
+      "AEST": "8:30pm",
+      "ACST": "8:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Sunday 20 July",
+    "stage": 15,
+    "distance": 222,
+    "location": "Tallard - Nîmes",
+    "time": {
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Monday 21 July",
+    "rest": true
+  },
+  {
+    "date": "Tuesday 22 July",
+    "stage": 16,
+    "distance": 237,
+    "location": "Carcassonne - Bagnères-de-Luchon",
+    "time": {
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Wednesday 23 July",
+    "stage": 17,
+    "distance": 125,
+    "location": "Saint-Gaudens - Saint-Lary Pla d’Adet",
+    "time": {
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Thursday 24 July",
+    "stage": 18,
+    "distance": 145,
+    "location": "Pau - Hautacam",
+    "time": {
+      "ONLINE": "9:00pm AEST",
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Friday 25 July",
+    "stage": 19,
+    "distance": 208,
+    "location": "Maubourguet Pays du Val’Adour - Bergerac",
+    "time": {
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Saturday 26 July",
+    "stage": 20,
+    "distance": 54,
+    "location": "Bergerac  - Périgeux",
+    "time": {
+      "AEST": "10:00pm",
+      "ACST": "9:30pm",
+      "AWST": "8:30pm"
+    }
+  },
+  {
+    "date": "Sunday 27 July",
+    "stage": 21,
+    "distance": 136,
+    "location": "Évry - Paris Champs-Élysées",
+    "time": {
+      "ONLINE": "9:00pm AEST",
+      "AEST": "11:00pm",
+      "ACST": " 10:30pm",
+      "AWST": "10:00pm"
+    }
+  }
+]
